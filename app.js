b6f44d5dedd1b6c1e62cebe88409b213ab825218
odamilola36 <odamilola36@gmail.com>
2020-08-12 10:07:35 +0100

    style completed

---
+++ app.js
@@ -0,0 +1,23 @@
+let button = document.querySelector("button");
+let ham = document.querySelector("#ham");
+let close = document.querySelector("#close");
+let nav = document.querySelector(".nav-links")
+
+ham.onclick = close.onclick = function(){
+    ham.classList.toggle("hide-mobile");
+    close.classList.toggle("hide-mobile");
+    nav.classList.toggle("show-nav");
+};
+
+button.disabled = window.pageYOffset === 0 ? true : false;
+
+button.onclick = function(){
+    if(!button.disabled){
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+    else return;
+}
